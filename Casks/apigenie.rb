0ae88dcd2768595412fe336be49852209b044a9c
Andriy Mykulyak <mykulyak@gmail.com>
2024-09-15 07:59:45 +0200

    Use signed binary - c.d. (#5)

--- Casks/apigenie.rb
+++ Casks/apigenie.rb
@@ -2,7 +2,9 @@ cask "apigenie" do
   version "0.8.0"
   sha256 "c95782d5d20ed1a631abf0505d8bd04906685e825244cd645f91dc18e00b499e"
 
-  url "https://apigenie.pl/dist/#{version}/apigenie-#{version}-macos_14-arm64.zip"
+  # url "https://apigenie.pl/dist/#{version}/apigenie-#{version}-macos_14-arm64.zip"
+  url "https://storage.googleapis.com/apigenie.pl/dist/#{version}/apigenie-#{version}-macos_14-arm64.zip",
+      verified: "storage.googleapis.com/apigenie.pl/"
   name "apigenie"
   desc "Best software for OpenAPI development"
   homepage "https://apigenie.pl/"
