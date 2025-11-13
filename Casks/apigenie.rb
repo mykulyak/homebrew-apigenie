ee4f9ac235f4ca89b9c6ae122d9bd401368ac77e
Andriy Mykulyak <mykulyak@gmail.com>
2025-11-13 20:11:37 +0100

    Fix checksum

--- Casks/apigenie.rb
+++ Casks/apigenie.rb
@@ -1,6 +1,6 @@
 cask "apigenie" do
   version "0.8.2"
-  sha256 "077c04cf1ed1dc484efa3cecf81cf99b6cf84eb97a6c37bca4fe5d0fb0912c62"
+  sha256 "e27b2208e7d0c5f0c41d44bfd882b92b67b75a17c2350016f9a5c529a7cca52c"
 
   # url "https://apigenie.pl/dist/#{version}/apigenie-#{version}-macos_14-arm64.zip"
   url "https://storage.googleapis.com/apigenie.pl/dist/#{version}/apigenie-#{version}-macos_15-arm64.zip",
