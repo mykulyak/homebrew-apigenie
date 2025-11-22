1c7aaf3d35e5fd4518c1fd8229e60167fc06a94a
Andriy Mykulyak <mykulyak@gmail.com>
2025-11-22 12:46:18 +0100

    Bump version to 0.8.5

--- Casks/apigenie.rb
+++ Casks/apigenie.rb
@@ -1,5 +1,5 @@
 cask "apigenie" do
-  version "0.8.4"
+  version "0.8.5"
   sha256 "4e6f9d88280799d871326f66337c619b2e405eb612193415e70ba9c2984492bf"
 
   url "https://storage.googleapis.com/apigenie.pl/dist/#{version}/apigenie-#{version}-macos15-arm64.zip",
